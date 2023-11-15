3328ed43d433eb7f3ce04b17b8b06ac3156ab7e7
alvarorojopelarda <alvarorp.150@gmail.com>
2023-11-15 16:34:20 +0100

    Update java.js

--- TrabajoPPT/java.js
+++ TrabajoPPT/java.js
@@ -1,16 +1,65 @@
- function empezarJuego() {
-        const jugador = document.getElementById("label1").value;
-        const rondas = prompt("Selecciona la cantidad de rondas (3 o 5):");
+let rondasJugadas = 0;
+let rondasTotales = 0;
+function empezarJuego() {
+       const jugador = document.getElementById("label1").value;
+       
+   
     
-        if (rondas === "3" || rondas === "5") {
-            alert("¡Bienvenido al juego " + jugador + "! Suerte, la vas a necesitar!");
-        } else {
-            alert("Seleccione un número de rondas válido");
-            return; 
-        }
-    
-        document.getElementById("botonJugar").disabled = false;
-        document.getElementById("botonEstadisticas").disabled = false;
-    }
+       document.getElementById("botonJugar").disabled = false;
+       document.getElementById("botonEstadisticas").disabled = false;
+   }
 
+ function jugarPartida(){
+     
+   const imagenPiedra = document.getElementById("Ipiedra");
+   const imagenPapel = document.getElementById("Ipapel");
+   const imagenTijera = document.getElementById("Itijera");
+   const botonPiedra = document.getElementById("piedra");
+   const botonPapel = document.getElementById("papel");
+   const botonTijera = document.getElementById("tijera");
 
+   imagenPiedra.hidden = false;
+   imagenPapel.hidden = false;
+   imagenTijera.hidden = false;
+   botonPapel.hidden = false;
+   botonPiedra.hidden = false;
+   botonTijera.hidden = false;
+
+   const rondasSeleccionadas = prompt("Selecciona la cantidad de rondas (3 o 5):");
+   if (rondasSeleccionadas === "3" || rondasSeleccionadas === "5"){
+     
+       rondasTotales = parseInt(rondasSeleccionadas);
+       for (let i = 0; i < rondasTotales; i++){
+           juego(jugador);
+       }
+       
+       
+   }else {
+       alert("Seleccione un número de rondas válido 3 o 5");
+       return;
+       
+     }
+ }
+
+ function juego(jugador){
+  
+   const choices = ["piedra", "papel", "tijera"];
+   const computer = choices[Math.floor(Math.random() * 3)];
+   
+   if ((jugador === "papel "&& computer === "piedra") || (jugador === "piedra" && computer === "tijera") || (jugador === "tijera" && computer === "papel")) {
+       alert("Ganaste"); 
+   }else if ((jugador === "piedra" && computer === "papel") || (jugador === "papel" && computer === "tijera") || (jugador === "tijera" && computer === "piedra")){
+       alert("Perdiste") ; 
+   }else{
+       alert("Empate"); 
+   }
+   
+   rondasJugadas++;
+
+   if (rondasJugadas === rondasTotales){
+       alert("Gracias por jugar");
+   }
+   document.getElementById("botonJugar").disabled = true;
+   document.getElementById("botonEstadisticas").disabled = true;
+  
+ }
